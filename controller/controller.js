1a8e76073d1586a13329f397992698e8baa74c17
Sajjad <sajjadmazharjr@gamil.com>
2021-12-15 19:58:24 +0530

    mysql crud

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,66 @@
 const knex=require("../config/db")
 
-insertData=(req,res)=>{
-    knex("user").insert(req.body).then((data)=>{
-        console.log("data insert");
-        res.send({massage:"data insert successfully"})
-    }).catch(err=>{
-        console.log(err);
-        res.send(err.massage)
+// const insertData=(req,res)=>{
+
+//     knex("user").insert(req.body).then((data)=>{
+//         console.log("data inserted");
+//         res.send({massage:"data insert successfully"})
+//     }).catch(err=>{
+//         console.log(err);
+//         res.send(err.massage)
+//     })
+// }
+
+exports.showData = (req, res)=>{
+    knex("user").then((rowData)=>{
+        res.send(rowData)
+    }).catch((err)=>{
+        console.log(err)
+        res.send({"status":"failed"})
     })
 }
 
-module.exports={insertData}
+exports.insertData = async (req, res)=>{
+    try{
+        let data = await knex("user")
+        let dataToInsert = await req.body
+        let exists = false;
+        for (let d of data){
+            if (d.name == dataToInsert.name){
+                exists = true
+            }
+        }
+        if (!exists){
+            await knex("user").insert(dataToInsert)
+            res.send({status:"inserted"})
+        }else{
+            res.send("duplicate entries not allowed!")
+        }
+
+    }catch(err){
+        console.log("some error occured: ",err.message)
+        res.send({status:"failed"})
+    }
+
+}
+
+exports.updateData = async(req, res)=>{
+    let id = req.params.id;
+    await knex("user").where({id}).update(req.body);
+    res.send("updated");
+
+}
+
+exports.deleteData = async(req, res)=>{
+    try{
+        let id = await req.params.id;
+        let deleted = await knex("user").where({id}).del();
+        console.log(deleted);
+        res.send({status:"deleted"});
+
+    }catch(err){
+        res.send({status:"data does not exist."});
+    }
+}
+
+// module.exports={insertData}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const express=require("express")
 const route=express.Router()
 
 
+route.get("/data", controller.showData)
 route.post("/insert",controller.insertData)
+route.put("/update/:id", controller.updateData)
+route.delete("/delete/:id", controller.deleteData)
 
 module.exports=route
